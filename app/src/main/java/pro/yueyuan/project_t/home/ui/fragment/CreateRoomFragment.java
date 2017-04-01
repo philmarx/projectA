11a12fc052b4e922afcb1a75a514c9cac0e2032c
徐强 <xq@yueyuan.pro>
2017-04-01 17:29:47 +0800

    创建房间—完成时间选择器

--- app/src/main/java/pro/yueyuan/project_t/home/ui/fragment/CreateRoomFragment.java
+++ app/src/main/java/pro/yueyuan/project_t/home/ui/fragment/CreateRoomFragment.java
@@ -1,15 +1,20 @@
 package pro.yueyuan.project_t.home.ui.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.BottomNavigationView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -20,6 +25,7 @@ import pro.yueyuan.project_t.BaseFragment;
 import pro.yueyuan.project_t.R;
 import pro.yueyuan.project_t.data.ShowGameListBean;
 import pro.yueyuan.project_t.home.IHomeContract;
+import pro.yueyuan.project_t.widget.CityPicker;
 import pro.yueyuan.project_t.widget.plugins.SelectData;
 
 import static dagger.internal.Preconditions.checkNotNull;
@@ -29,18 +35,32 @@ import static dagger.internal.Preconditions.checkNotNull;
  */
 
 public class CreateRoomFragment extends BaseFragment implements IHomeContract.View {
+
+    private PopupWindow mCityPop;
+    private View cityPopView,mDatePopView;
+    private CityPicker cityPicker;
+    private static String mSheng,mShi,mquxian;
+    public static String city_all="";
+    private TextView Select_Ok,Select_Cancel;
     @BindView(R.id.rl_createroom_starttime_fmt)
     RelativeLayout rl_createroom_starttime_fmt;
     @BindView(R.id.rl_createroom_endtime_fmt)
     RelativeLayout rl_createroom_endtime_fmt;
-    @BindView(R.id.et_createroom_starttime_fmt)
-    EditText et_createroom_starttime_fmt;
-    @BindView(R.id.et_createroom_endttime_fmt)
-    TextView et_createroom_endttime_fmt;
+    @BindView(R.id.tv_createroom_starttime_fmt)
+    TextView tv_createroom_starttime_fmt;
+    @BindView(R.id.tv_createroom_endttime_fmt)
+    TextView tv_createroom_endttime_fmt;
+    @BindView(R.id.rl_createroom_gametype_fmt)
+    RelativeLayout rl_createroom_gametype_fmt;
+    @BindView(R.id.tv_createroom_gametype_fmt)
+    TextView tv_createroom_gametype_fmt;
+
     /**
      * 通过重写第一级基类IBaseView接口的setPresenter()赋值
      */
     private IHomeContract.Presenter mPresenter;
+
+    private SelectData selectData;
 
     public BottomNavigationView bottomNavigationView;
 
@@ -65,22 +85,34 @@ public class CreateRoomFragment extends BaseFragment implements IHomeContract.Vi
 
     @OnClick({
             R.id.rl_createroom_starttime_fmt,
-            R.id.rl_createroom_endtime_fmt
+            R.id.rl_createroom_endtime_fmt,
+            R.id.rl_createroom_gametype_fmt
     })
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.rl_createroom_starttime_fmt:
-                SelectData selectData  = new SelectData(getActivity(),true);
-                selectData.showAtLocation(et_createroom_starttime_fmt, Gravity.BOTTOM,0,0);
+                selectData = new SelectData(getActivity(), true);
+                selectData.showAtLocation(tv_createroom_starttime_fmt, Gravity.BOTTOM, 0, 0);
                 selectData.setDateClickListener(new SelectData.OnDateClickListener() {
                     @Override
                     public void onClick(String year, String month, String day, String hour, String minute) {
-                        Toast.makeText(getActivity(),
-                                year + "-" + month + "-" + day+" "+hour+":"+minute,
-                                Toast.LENGTH_LONG).show();
-                        et_createroom_starttime_fmt.setText(year + "-" + month + "-" + day+" "+hour+":"+minute);
+                        tv_createroom_starttime_fmt.setText(year + "-" + month + "-" + day + " " + hour + ":" + minute);
                     }
                 });
+                break;
+            case R.id.rl_createroom_endtime_fmt:
+                selectData = new SelectData(getActivity(), true);
+                selectData.showAtLocation(tv_createroom_endttime_fmt, Gravity.BOTTOM, 0, 0);
+                selectData.setDateClickListener(new SelectData.OnDateClickListener() {
+                    @Override
+                    public void onClick(String year, String month, String day, String hour, String minute) {
+                        tv_createroom_endttime_fmt.setText(year + "-" + month + "-" + day + " " + hour + ":" + minute);
+                    }
+                });
+                break;
+            case R.id.rl_createroom_gametype_fmt:
+                backgroundAlpha(0.3f);
+                showSelectionCityPOP(rl_createroom_gametype_fmt);
                 break;
         }
     }
@@ -94,6 +126,7 @@ public class CreateRoomFragment extends BaseFragment implements IHomeContract.Vi
     protected void initView(Bundle savedInstanceState) {
         bottomNavigationView = (BottomNavigationView) getActivity().findViewById(R.id.navigation_bottom);
         bottomNavigationView.setVisibility(View.GONE);
+        selectionCityPOP(R.layout.select_city_pop_main_layout);
     }
 
     @Override
@@ -106,11 +139,95 @@ public class CreateRoomFragment extends BaseFragment implements IHomeContract.Vi
 
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO: inflate a fragment view
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
-        ButterKnife.bind(this, rootView);
-        return rootView;
+
+    private void showSelectionCityPOP(View view) {
+        if (!mCityPop.isShowing()) {
+            mCityPop.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+        }
     }
+
+    /**
+     * 设置添加屏幕的背景透明度
+     * @param bgAlpha
+     */
+    public void backgroundAlpha(float bgAlpha){
+        WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
+        lp.alpha = bgAlpha; //0.0-1.0
+        getActivity().getWindow().setAttributes(lp);
+    }
+
+    /**
+     * 选择城市 自定义控件
+     *
+     * @param resId
+     */
+    private void selectionCityPOP(int resId) {
+        LayoutInflater mLayoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        cityPopView = mLayoutInflater.inflate(resId, null);
+        mCityPop = new PopupWindow(cityPopView, ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        // 必须设置background才能消失
+        mCityPop.setBackgroundDrawable(getResources().getDrawable(
+                R.drawable.fullscreen_share_bg));
+        mCityPop.setOutsideTouchable(true);
+        // 自定义动画
+        mCityPop.setAnimationStyle(R.style.AnimationBottomFade);
+        // 使用系统动画
+        //mCityPop.setAnimationStyle(R.style.mypopwindow_anim_style);
+
+        cityPicker=(CityPicker) cityPopView.findViewById(R.id.citypicker);
+        Select_Ok=(TextView) cityPopView.findViewById(R.id.Select_City_Ok);
+        Select_Cancel=(TextView) cityPopView.findViewById(R.id.Select_City_Cancel);
+        cityPicker.setCity(new CityPicker.testCity() {
+            @Override
+            public void cityAll(String sheng, String shi) {
+                // TODO Auto-generated method stub
+                mSheng=sheng;
+                mShi=shi;
+                handler.sendEmptyMessage(1);
+            }
+        });
+
+        Select_Ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                city_all=mSheng+"  "+mShi;
+                tv_createroom_gametype_fmt.setText(""+city_all);
+                mCityPop.dismiss();
+            }
+        });
+
+        Select_Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                city_all="";
+                mCityPop.dismiss();
+            }
+        });
+        // popWindow消失监听方法
+        mCityPop.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                backgroundAlpha(1f);
+            }
+        });
+
+        mCityPop.update();
+        mCityPop.setTouchable(true);
+        mCityPop.setFocusable(true);
+    }
+
+    public static Handler handler=new Handler(){
+        public void handleMessage(android.os.Message msg) {
+
+            switch (msg.what) {
+                case 1:
+					/*sheng_Text.setText(mSheng+"");
+					shi_Text.setText(mShi+"");*/
+                    break;
+                default:
+                    break;
+            }
+        };
+    };
 }
