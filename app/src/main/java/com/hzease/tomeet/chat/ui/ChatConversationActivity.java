05aaf91f20b3b40577d91be4f67f3c6e9f1f1602
徐强 <xq@yueyuan.pro>
2017-05-16 11:38:54 +0800

    头像

--- app/src/main/java/com/hzease/tomeet/chat/ui/ChatConversationActivity.java
+++ app/src/main/java/com/hzease/tomeet/chat/ui/ChatConversationActivity.java
@@ -3,11 +3,16 @@ package com.hzease.tomeet.chat.ui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.hzease.tomeet.AppConstants;
 import com.hzease.tomeet.NetActivity;
+import com.hzease.tomeet.PTApplication;
 import com.hzease.tomeet.R;
+import com.hzease.tomeet.data.RealmFriendBean;
 import com.orhanobut.logger.Logger;
 
+import io.realm.Realm;
 import io.rong.imkit.RongIM;
 import io.rong.imlib.IRongCallback;
 import io.rong.imlib.RongIMClient;
@@ -20,6 +25,8 @@ import io.rong.message.LocationMessage;
  * 隐式意图启动的会话列表，融云自带的界面
  */
 public class ChatConversationActivity extends NetActivity {
+
+    private String targetId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,18 +46,25 @@ public class ChatConversationActivity extends NetActivity {
      */
     @Override
     protected void initLayout(Bundle savedInstanceState) {
-        /*RongIM.setUserInfoProvider(new RongIM.UserInfoProvider() {
+        targetId = getIntent().getData().getQueryParameter("targetId");
+
+        RealmFriendBean friendBean = Realm.getDefaultInstance().where(RealmFriendBean.class).equalTo("id", Long.valueOf(targetId)).findFirst();
+
+        final UserInfo userInfo = new UserInfo(targetId, friendBean.getNickname(), Uri.parse(AppConstants.YY_PT_OSS_USER_PATH + targetId + AppConstants.YY_PT_OSS_AVATAR_THUMBNAIL + "#" + friendBean.getAvatarSignature()));
+        final UserInfo myInfo = new UserInfo(PTApplication.userId, PTApplication.myInfomation.getData().getNickname(), Uri.parse(AppConstants.YY_PT_OSS_USER_PATH_MYSELF + AppConstants.YY_PT_OSS_AVATAR_THUMBNAIL + "#" + PTApplication.myInfomation.getData().getAvatarSignature()));
+        Logger.e("getUserInfo" + targetId + "    nick:  " + friendBean.getNickname() + "\n" + AppConstants.YY_PT_OSS_USER_PATH + targetId + AppConstants.YY_PT_OSS_AVATAR_THUMBNAIL + "#" + friendBean.getAvatarSignature());
+
+        RongIM.setUserInfoProvider(new RongIM.UserInfoProvider() {
             @Override
-            public UserInfo getUserInfo(String s) {
-                return null;
+            public UserInfo getUserInfo(String userId) {
+                return userId.equals(PTApplication.userId) ? myInfo : userInfo;
             }
-        }, false);*/
+        }, false);
     }
 
 
 
     /**
-     * TODO 调用 mRequestService 获取网络参数去初始化布局
      *
      * @param savedInstanceState
      */
@@ -70,7 +84,7 @@ public class ChatConversationActivity extends NetActivity {
         double lng = data.getDoubleExtra("lng",100.00);
         String poi = data.getStringExtra("poi");
         Logger.e("uri" + uri +"\nlat" + lat + "\nlng" + lng + "\npoi" + poi);
-        String targetId = getIntent().getData().getQueryParameter("targetId");
+
         LocationMessage message = LocationMessage.obtain(lat,lng,poi,Uri.parse(uri));
         Message sendMessage = Message.obtain(targetId, Conversation.ConversationType.PRIVATE,message);
         RongIM.getInstance().sendLocationMessage(sendMessage, null, null, new IRongCallback.ISendMediaMessageCallback() {
