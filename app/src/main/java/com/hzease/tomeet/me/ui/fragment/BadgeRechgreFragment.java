5d0973aecb56a9099bc0200ffcad0a56352364da
徐强 <xq@yueyuan.pro>
2017-05-18 20:20:12 +0800

    充值徽章

---
+++ app/src/main/java/com/hzease/tomeet/me/ui/fragment/BadgeRechgreFragment.java
@@ -0,0 +1,219 @@
+package com.hzease.tomeet.me.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import com.hzease.tomeet.BaseFragment;
+import com.hzease.tomeet.R;
+import com.hzease.tomeet.data.BadgeBean;
+import com.hzease.tomeet.data.GameFinishBean;
+import com.hzease.tomeet.data.MyJoinRoomsBean;
+import com.hzease.tomeet.data.WaitEvaluateBean;
+import com.hzease.tomeet.me.IMeContract;
+import com.hzease.tomeet.utils.ToastUtils;
+import com.orhanobut.logger.Logger;
+import com.zhy.autolayout.AutoLinearLayout;
+import com.zhy.view.flowlayout.FlowLayout;
+import com.zhy.view.flowlayout.TagAdapter;
+import com.zhy.view.flowlayout.TagFlowLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static dagger.internal.Preconditions.checkNotNull;
+
+/**
+ * Created by xuq on 2017/5/18.
+ */
+
+public class BadgeRechgreFragment extends BaseFragment implements IMeContract.View {
+
+    @BindView(R.id.tfl_badge_regchre_fmt)
+    TagFlowLayout tfl_badge_regchre_fmt;
+    @BindView(R.id.cb_badge_alipay_fmt)
+    CheckBox cb_badge_alipay_fmt;
+    @BindView(R.id.cb_badge_wxpay_fmt)
+    CheckBox cb_badge_wxpay_fmt;
+    @BindView(R.id.bt_me_changpwd_fmt)
+    Button bt_me_changpwd_fmt;
+    /**
+     * 通过重写第一级基类IBaseView接口的setPresenter()赋值
+     */
+    private IMeContract.Presenter mPresenter;
+    private String[] badgeCount = {"5", "10", "20", "30", "50", "100"};
+    private String[] badgeMoneys = {"5元", "10元", "20元", "30元", "50元", "100元"};
+    private List<BadgeBean> mDatas;
+    private String pay;
+    private String count;
+    private String money;
+
+    @OnClick({
+            R.id.bt_me_changpwd_fmt
+    })
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.bt_me_changpwd_fmt:
+                //TODO 执行充值操作
+                if (cb_badge_alipay_fmt.isChecked()){
+                    ToastUtils.getToast(getContext(),pay + money + "元,获得徽章" + count + "枚");
+                }else{
+                    ToastUtils.getToast(getContext(),pay + money + "元,获得徽章" + count + "枚");
+                }
+                getActivity().getSupportFragmentManager().popBackStack();
+                break;
+        }
+    }
+    @Override
+    public void setPresenter(IMeContract.Presenter presenter) {
+        mPresenter = checkNotNull(presenter);
+    }
+
+    public static BadgeRechgreFragment newInstance() {
+        return new BadgeRechgreFragment();
+    }
+
+    /**
+     * 显示 或 刷新 我的信息
+     */
+    @Override
+    public void showMyInfo() {
+
+    }
+
+    /**
+     * 显示我的房间
+     *
+     * @param myJoinRoomBean
+     * @param isLoadMore
+     */
+    @Override
+    public void showMyRooms(MyJoinRoomsBean myJoinRoomBean, boolean isLoadMore) {
+
+    }
+
+    /**
+     * 更新密码成功
+     *
+     * @param isSuccess
+     * @param msg
+     */
+    @Override
+    public void updatePwdSuccess(boolean isSuccess, String msg) {
+
+    }
+
+    /**
+     * 提交反馈成功
+     *
+     * @param isSuccess
+     * @param msg
+     */
+    @Override
+    public void feedBackSuccess(boolean isSuccess, String msg) {
+
+    }
+
+    /**
+     * 认证成功
+     */
+    @Override
+    public void authorizedSuccess() {
+
+    }
+
+    /**
+     * 显示结束房间信息
+     *
+     * @param data
+     */
+    @Override
+    public void showFinishInfo(GameFinishBean.DataBean data) {
+
+    }
+
+    /**
+     * 显示待评价成员
+     *
+     * @param data
+     */
+    @Override
+    public void showWaitEvaluateMember(List<WaitEvaluateBean.DataBean> data) {
+
+    }
+
+    /**
+     * @return 布局文件ID
+     */
+    @Override
+    public int getContentViewId() {
+        return R.layout.fragment_badgerechgre;
+    }
+
+    /**
+     * TODO 初始化布局文件
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        initDatas();
+        tfl_badge_regchre_fmt.setAdapter(new TagAdapter<BadgeBean>(mDatas) {
+            @Override
+            public View getView(FlowLayout parent, int position, BadgeBean badgeBean) {
+                AutoLinearLayout view = (AutoLinearLayout) View.inflate(getContext(), R.layout.item_taglayout, null);
+                TextView badgeCount = (TextView) view.findViewById(R.id.tv_badge_count_fmt);
+                TextView badgeMoney = (TextView) view.findViewById(R.id.tv_badge_money_fmt);
+                badgeCount.setText(mDatas.get(position).getBadgeCount());
+                badgeMoney.setText(mDatas.get(position).getBadgeMoney());
+                return view;
+            }
+        });
+        tfl_badge_regchre_fmt.setOnTagClickListener(new TagFlowLayout.OnTagClickListener() {
+            @Override
+            public boolean onTagClick(View view, int position, FlowLayout parent) {
+                count = mDatas.get(position).getBadgeCount();
+                money = mDatas.get(position).getBadgeMoney();
+                Logger.e(position+"\ncount " + count + "\nmoney" + money);
+                return true;
+            }
+        });
+        cb_badge_alipay_fmt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    cb_badge_wxpay_fmt.setChecked(false);
+                    pay = "支付宝充值";
+                }
+            }
+        });
+        cb_badge_wxpay_fmt.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                cb_badge_alipay_fmt.setChecked(false);
+                pay = "微信充值"+ money + "元,获得徽章" + count + "枚";
+            }
+        });
+    }
+
+    //加载对象
+    private void initDatas() {
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            BadgeBean bean = new BadgeBean();
+            bean.setBadgeCount(badgeCount[i]);
+            bean.setBadgeMoney(badgeMoneys[i]);
+            mDatas.add(bean);
+        }
+    }
+
+}
